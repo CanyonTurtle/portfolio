f4340ae5400e97614df53c6b2f05156f09c79a48
Cannon <cannonator007@gmail.com>
2025-06-23 15:44:18 -0600

    test out

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['next-mdx-remote'],
+  basePath: isProd ? '' : '', 
+  assetPrefix: isProd ? '/' : '', 
+  images: { unoptimized: true}, // GitHub Pages does not support Next.js image optimization 
+  output: "export"
 };
 
 export default nextConfig;
---
+++ blog/out/_next/static/chunks/app/blog/[slug]/page-e11aee85665f5204.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[953],{1236:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,371,23)),Promise.resolve().then(n.t.bind(n,1092,23))}},e=>{e.O(0,[371,92,503,122,358],()=>e(e.s=1236)),_N_E=e.O()}]);
