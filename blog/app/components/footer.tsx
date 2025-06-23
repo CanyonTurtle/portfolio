855911b7467054352557764c827d17d629d654a0
Cannon <cannonator007@gmail.com>
2025-06-23 15:11:50 -0600

    wip

--- blog/app/components/footer.tsx
+++ blog/app/components/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             href="/rss"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">linkedin</p>
+            <p className="ml-2 h-7">rss</p>
           </a>
         </li>
         <li>
@@ -39,6 +39,17 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/cannontuttle/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
       </ul>
