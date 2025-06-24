733d704360d6c44990fa04edb1f253ce8337f0c6
Cannon <cannonator007@gmail.com>
2025-06-24 15:21:55 -0600

    no asset prefix

--- base-path.ts
+++ base-path.ts
@@ -2,4 +2,4 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const basePath = isProd ? '/portfolio' : '';
-export const assetPrefix = isProd ? '/portfolio' : '';
+export const assetPrefix = undefined;
