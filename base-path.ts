7e0f554a801d5f6966df7fe71100e948a81ead8d
Cannon <cannonator007@gmail.com>
2025-06-24 16:21:27 -0600

    wip

--- base-path.ts
+++ base-path.ts
@@ -7,6 +7,7 @@ export const assetPrefix = undefined;
 
 export const baseUrl = `${baseUri}${basePath}`;
 
+/** https://www.reddit.com/r/nextjs/comments/1ixfypv/nextjs_on_github_pages/ */
 export const getImageFullPath = (path: string) => {
 
   if (process.env.NODE_ENV === 'production') {
