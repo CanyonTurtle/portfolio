3b68895ff0e8d2ad8aea619258360994345e39fd
Cannon <cannonator007@gmail.com>
2025-06-24 13:15:07 -0600

    wip

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-
+console.log("isProd\n\n\n\n\n", isProd, process.env.NODE_ENV, "\n\n\n\n\n")
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['next-mdx-remote'],
-  basePath: isProd ? '/canyonturtle.github.io/portfolio' : '', 
-  assetPrefix: isProd ? '/canyonturtle.github.io/portfolio' : '', 
+  basePath: isProd ? '/portfolio' : '', 
+  assetPrefix: isProd ? '/portfolio' : '', 
   images: { unoptimized: true}, // GitHub Pages does not support Next.js image optimization 
   output: "export"
 };
