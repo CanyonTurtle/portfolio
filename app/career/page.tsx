b642c793e8f4da38d8aadeb856306e47bcfa6383
Cannon <cannonator007@gmail.com>
2025-06-24 21:14:13 -0600

    link

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My career</h1>
       <p className="mb-8 text-neutral-700 dark:text-neutral-300 leading-relaxed">
-        For detailed resume information, see <Link href="https://www.linkedin.com/in/cannontuttle/#:~:text=Show%20all%20posts-,Experience,-Experience" className="text-blue-600 hover:text-blue-700">my LinkedIn profile</Link>. Here you'll find more details about my roles, responsibilities, and accomplishments.
+        For detailed resume information, see <Link href="https://www.linkedin.com/in/cannontuttle/#:~:text=Show%20all%20posts-,Experience,-Experience" className="hover:underline">my LinkedIn profile</Link>. Here you'll find more details about my roles, responsibilities, and accomplishments.
       </p>
       <CareerList />
     </section>
