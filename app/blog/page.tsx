9ac1b664314506842d2d43706c75954b4204e943
Cannon <cannonator007@gmail.com>
2025-06-24 21:20:58 -0600

    blog funny

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,7 +10,11 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My blog</h1>
       <p className="mb-8 text-neutral-700 dark:text-neutral-300 leading-relaxed">
-        Thoughts, tutorials, and insights from my journey in software engineering. Here you'll find technical deep-dives, learning experiences, and reflections on building software.
+      Jim: "Last year, Creed asked me how to setup a blog. Wanting to protect the world from being exposed to Creed's brain, I opened up a Word document on his computer and put an address at the top."
+      <br />
+      Creed: "www.creedthoughts.gov.www/creedthoughts"
+      <br />
+      - The Office
       </p>
       <BlogPosts />
     </section>
